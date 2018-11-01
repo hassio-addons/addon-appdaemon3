53224fa2c228b39c4ec90d5c1d02e6d6d20faffb
Franck Nijhof <frenck@addons.community>
2018-11-01 09:05:54 +0100

    :sparkles: Adds logic to automatically flush compiled cache on version upgrade

--- appdaemon/rootfs/etc/cont-init.d/21-compiled-dir.sh
+++ appdaemon/rootfs/etc/cont-init.d/21-compiled-dir.sh
@@ -6,8 +6,29 @@
 # shellcheck disable=SC1091
 source /usr/lib/hassio-addons/base.sh
 
-mkdir -p \
-    /data/compiled/css \
-    /data/compiled/html \
-    /data/compiled/javascript \
-    || hass.die 'Failed to created compiled directory'
+if hass.directory_exists '/data/compiled'; then
+
+    # Deleted compiled cache, in case we don't know the version
+    if ! hass.file_exists '/data/version'; then
+        hass.log.info "Detected version upgrade, clearing compiled cache..."
+        rm -fr /data/compiled
+    fi
+
+    # Given version does not match
+    if hass.file_exists '/data/version' \
+        && [[ "$(</data/version)" != "$(hass.addon.version)" ]];
+    then
+        hass.log.info "Detected version upgrade, clearing compiled cache..."
+        rm -fr /data/compiled
+    fi
+fi
+
+if ! hass.directory_exists '/data/compiled'; then
+    mkdir -p \
+        /data/compiled/css \
+        /data/compiled/html \
+        /data/compiled/javascript \
+        || hass.die 'Failed to created compiled directory'
+
+    echo -e "$(hass.addon.version)" > /data/version
+fi
